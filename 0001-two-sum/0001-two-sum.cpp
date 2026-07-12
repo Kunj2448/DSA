0dba979d2623e88b6e52d7d8ffc62c309698b9d1
Kunj2448 <vaghanikunj2448@gmail.com>
2026-07-12 16:44:27 +0530

    Time: 0 ms (100%), Space: 14.8 MB (37.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,15 +5,14 @@ public:
         int n=nums.size();
 
         for(int i=0;i<n;i++){
-            int first=nums[i];
-            int second=target-first;
+            int second=target-nums[i];
 
             if(m.find(second)!=m.end()){
                 return {i,m[second]};
                 break;
             }
-            m[first]=i;
+            m[nums[i]]=i;
         }
-        return {-1,-1};
+        return {};
     }
 };
