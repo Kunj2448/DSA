125142b2f9892d99a80e888c98a740ab5ad1a800
Kunj2448 <vaghanikunj2448@gmail.com>
2026-07-12 16:54:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (72.89%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -3,7 +3,7 @@ public:
     int divide(int dividend, int divisor) {
         if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
-        double ans=dividend/divisor;
-        return int(ans);
+        
+        return dividend/divisor;
     }
 };
